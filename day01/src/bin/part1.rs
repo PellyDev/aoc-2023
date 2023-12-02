9f84bebac7c8b77fada0aa5712b4cf83ca07d57d
Pelly <zabookinkajou@protonmail.com>
2023-12-02 23:13:19 +0100

    remove except and implement fallback for no digit in line

--- day01/src/bin/part1.rs
+++ day01/src/bin/part1.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let input = include_str!("../sample_input.txt");
+    let input = include_str!("../my_input.txt");
     let res = input
         .lines()
         .map(|line| {
             let mut iter = line.chars().filter_map(|c| c.to_digit(10));
-            let first = iter
-                .next()
-                .expect(format!("No number found in line '{}'", line).as_str())
-                * 10;
+            let first = iter.next().unwrap_or(0) * 10;
             let last = iter.last().unwrap_or(first / 10);
             first + last
         })
